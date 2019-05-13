255a9c66e49083cfd5843b9c426d56c3c7c4f2e4
Atnanasi <ys2000pro@gmail.com>
2019-05-13 19:27:17 +0900

    バリデーション追加

--- src/seaClient.ts
+++ src/seaClient.ts
@@ -1,43 +1,93 @@
+import $ from 'cafy'
 import axios, { AxiosInstance } from 'axios'
 
 /**
- * Account
+ * Validate date string
  */
-export interface Account {
-  createdAt: string
-  id: number
-  name: string
-  postsCount: number
-  screenName: string
-  updatedAt: string
-}
+const isValidDate = $.str.pipe(
+  (str: string): boolean => !isNaN(new Date(str).getTime())
+)
 
 /**
  * Application
  */
-export interface Application {
-  id: number
-  name: string
+export class Application {
+  public id: number
+  public name: string
+
+  /**
+   * Validate
+   *
+   * @param {Partial<Application>} application object
+   */
+  private validate(application: Partial<Application>) {
+    return $.obj({
+      id: $.num,
+      name: $.str
+    }).throw(application)
+  }
+
+  /**
+   * Constructor
+   *
+   * @param {Partial<Application>} a object
+   */
+  constructor(a: Partial<Application>) {
+    const application = this.validate(a)
+
+    this.id = application.id
+    this.name = application.name
+  }
 }
 
 /**
  * Authorization
  */
-export interface Authorization {
-  access_token: string
-  clientId: string
-  clientSecret: string
-  stateText: string
-  token_type: string
+export class Authorization {
+  public access_token: string
+  public clientId: string
+  public clientSecret: string
+  public stateText: string
+  public token_type: string
+
+  /**
+   * Validate
+   *
+   * @param {Partial<Authorization>} authorization object
+   */
+  private validate(authorization: Partial<Authorization>) {
+    return $.obj({
+      access_token: $.str,
+      clientId: $.str,
+      clientSecret: $.str,
+      stateText: $.str,
+      token_type: $.str
+    }).throw(authorization)
+  }
+
+  /**
+   * Constructor
+   *
+   * @param {Partial<Authorization>} a object
+   */
+  constructor(a: Partial<Authorization>) {
+    const authorization = this.validate(a)
+
+    this.access_token = authorization.access_token
+    this.clientId = authorization.clientId
+    this.clientSecret = authorization.clientSecret
+    this.stateText = authorization.stateText
+    this.token_type = authorization.token_type
+  }
 }
 
 /**
  * File
  */
-export interface File {
-  id: number
-  name: string
-  variants: {
+export class File {
+  public id: number
+  public name: string
+  public variants: {
     id: number
     score: number
     extension: string
@@ -45,47 +95,199 @@ export interface File {
     size: number
     url: string
     mime: string
+  }[]
+
+  /**
+   * Validate
+   *
+   * @param {Partial<File>} file object
+   */
+  private validate(file: Partial<File>) {
+    return $.obj({
+      id: $.num,
+      name: $.str,
+      variants: $.array(
+        $.obj({
+          id: $.num,
+          score: $.num,
+          extension: $.str,
+          type: $.str,
+          size: $.num,
+          url: $.str,
+          mime: $.str
+        })
+      )
+    }).throw(file)
+  }
+
+  /**
+   * Constructor
+   *
+   * @param {Partial<File>} f object
+   */
+  constructor(f: Partial<File>) {
+    const file = this.validate(f)
+
+    this.id = file.id
+    this.name = file.name
+    this.variants = file.variants
   }
 }
 
 /**
  * Post
  */
-export interface Post {
-  application: Application
-  createdAt: string
-  id: number
-  text: string
-  updatedAt: string
-  user: User
-  files: File[]
+export class Post {
+  public application: Application
+  public createdAt: Date
+  public id: number
+  public text: string
+  public updatedAt: Date
+  public user: User
+  public files: File[]
+
+  /**
+   * Validate
+   *
+   * @param {Partial<Post>} post object
+   */
+  private validate(post: Partial<Post>) {
+    return $.obj({
+      application: $.any,
+      createdAt: isValidDate,
+      id: $.num,
+      text: $.str,
+      updatedAt: isValidDate,
+      user: $.any,
+      files: $.arr($.any)
+    }).throw(post)
+  }
+
+  /**
+   * Make Account object
+   *
+   * @param {Partial<Post>} p object
+   */
+  constructor(p: Partial<Post>) {
+    const post = this.validate(p)
+
+    this.application = new Application(post.application)
+    this.createdAt = new Date(post.createdAt)
+    this.id = post.id
+    this.text = post.text
+    this.updatedAt = new Date(post.updatedAt)
+    this.user = new User(post.user)
+    this.files = []
+
+    post.files.forEach(file => {
+      this.files.push(new File(file))
+    })
+  }
 }
 
 /**
  * Post body
  */
-export interface PostBody {
-  text: string
-  fileIds?: number[]
+export class PostBody {
+  public text: string
+  public fileIds?: number[]
+
+  /**
+   * Validate
+   *
+   * @param {Partial<PostBody>} postBody object
+   */
+  private validate(postBody: Partial<PostBody>) {
+    return $.obj({
+      text: $.str,
+      fileIds: $.optional.array($.num)
+    }).throw(postBody)
+  }
+
+  /**
+   * Constructor
+   *
+   * @param {Partial<PostBody>} p object
+   */
+  constructor(p: Partial<PostBody>) {
+    const postBody = this.validate(p)
+
+    this.text = postBody.text
+    this.fileIds = postBody.fileIds
+  }
 }
 
 /**
- * User
+ * User class
  */
-export interface User {
-  createdAt: string
-  id: number
-  name: string
-  postsCount: number
-  screenName: string
-  updatedAt: string
+export class User {
+  public createdAt: Date
+  public id: number
+  public name: string
+  public postsCount: number
+  public screenName: string
+  public updatedAt: Date
+
+  /**
+   * Validate
+   *
+   * @param {Partial<User>} account object
+   */
+  private validate(account: Partial<User>) {
+    return $.obj({
+      createdAt: isValidDate,
+      id: $.num,
+      name: $.str,
+      postsCount: $.num,
+      screenName: $.str,
+      updatedAt: isValidDate
+    }).throw(account)
+  }
+
+  /**
+   * Constructor
+   *
+   * @param {Partial<User>} a object
+   */
+  constructor(a: Partial<User>) {
+    const account = this.validate(a)
+
+    this.createdAt = new Date(account.createdAt)
+    this.id = account.id
+    this.name = account.name
+    this.postsCount = account.postsCount
+    this.screenName = account.screenName
+    this.updatedAt = new Date(account.updatedAt)
+  }
 }
 
 /**
  * User settings
  */
-export interface UserSettings {
-  name: string
+export class UserSettings {
+  public name: string
+
+  /**
+   * Validate
+   *
+   * @param {Partial<UserSettings>} userSettings object
+   */
+  private validate(userSettings: Partial<UserSettings>) {
+    return $.obj({
+      name: $.str
+    }).throw(userSettings)
+  }
+
+  /**
+   * Constructor
+   *
+   * @param {Partial<UserSettings>} p object
+   */
+  constructor(u: Partial<UserSettings>) {
+    const userSettings = this.validate(u)
+
+    this.name = userSettings.name
+  }
 }
 
 /**
@@ -122,13 +324,13 @@ export default class SeaClient {
   ) {
     this.endpoint = new URL(`https://${endpoint}`).href
 
-    this.auth = <Authorization>{
+    this.auth = new Authorization({
       access_token: accessToken || '',
       clientId: clientId || '',
       clientSecret: clientSecret || '',
       stateText: this.getStateText(),
       token_type: tokenType || 'Bearer'
-    }
+    })
 
     this.axios = axios.create({
       baseURL: this.endpoint
@@ -158,6 +360,8 @@ export default class SeaClient {
    * @returns {string}
    */
   public getAuthorizeUrl(): string {
+    if (this.auth.clientId === '') throw Error('clientId is not set')
+
     return new URL(
       `/oauth/authorize?client_id=${
         this.auth.clientId
@@ -198,14 +402,14 @@ export default class SeaClient {
   /**
    * Post
    *
-   * @param {Body} post post body
+   * @param {PostBody} post post body
    *
    * @returns {Promise<Post>}
    */
-  public async post(post: Body): Promise<Post> {
+  public async post(post: PostBody): Promise<Post> {
     const res = await this.axios.post<Post>('/api/v1/posts', post)
 
-    return res.data
+    return new Post(res.data)
   }
 
   /**
@@ -217,14 +421,14 @@ export default class SeaClient {
    * @returns {Promise<Post[]>}
    */
   public async getTimeline(sinceId?: number, count?: number): Promise<Post[]> {
-    const res = await this.axios.get<Post[]>(`/api/v1/timelines/public`, {
+    const res = await this.axios.get(`/api/v1/timelines/public`, {
       params: {
         sinceId,
         count
       }
     })
 
-    return res.data
+    return res.data.map((post: Post) => new Post(post))
   }
 
   /**
@@ -237,7 +441,7 @@ export default class SeaClient {
   public async postUserSettings(setting: UserSettings): Promise<User> {
     const res = await this.axios.patch<User>(`/api/v1/account`, setting)
 
-    return res.data
+    return new User(res.data)
   }
 
   /**
@@ -248,6 +452,6 @@ export default class SeaClient {
   public async getMyInfo(): Promise<User> {
     const res = await this.axios.get<User>(`/api/v1/account`)
 
-    return res.data
+    return new User(res.data)
   }
 }

428b173aa208622f22f426574ca77a2d417aca9b
Atnanasi <ys2000pro@gmail.com>
2019-05-20 16:04:42 +0900

    hai

--- src/index.ts
+++ src/index.ts
@@ -7,41 +7,48 @@ import Hydrobond, { Authorization } from './hydrobond/hydrobond'
  */
 const main = async () => {
   // Config
-  const ENDPOINT = new URL(process.env.TORPEDO_SEA_ENDPOINT || '')
-  const AUTHORIZATION = new Authorization({
-    accessToken: process.env.TORPEDO_ACCESS_TOKEN || '',
-    clientId: process.env.TORPEDO_CLIENT_ID || '',
-    clientSecret: process.env.TORPEDO_CLIENT_SECRET || ''
-  })
+  const CONFIG = {
+    API_ENDPOINT: new URL(process.env.TORPEDO_SEA_API_ENDPOINT || ''),
+    OAUTH_ENDPOINT: new URL(process.env.TORPEDO_SEA_OAUTH_ENDPOINT || ''),
+    AUTHORIZATION: new Authorization({
+      accessToken: process.env.TORPEDO_ACCESS_TOKEN || '',
+      clientId: process.env.TORPEDO_CLIENT_ID || '',
+      clientSecret: process.env.TORPEDO_CLIENT_SECRET || ''
+    })
+  }
 
   // Make Hydrobond instance
-  const hydrobond = new Hydrobond(ENDPOINT, AUTHORIZATION)
+  const hydrobond = new Hydrobond(
+    CONFIG.API_ENDPOINT,
+    CONFIG.OAUTH_ENDPOINT,
+    CONFIG.AUTHORIZATION
+  )
 
   // Parse command line
   const cli = cac()
   cli.help()
 
   // Authorize
-  cli.command('authorize', 'Authorize torpedo').action(() => {
-    authorize(hydrobond)
+  cli.command('authorize', 'Authorize torpedo').action(async () => {
+    await authorize(hydrobond)
   })
 
   // Post
   cli
     .command('post [text]', 'Post to sea')
     .option('--file <file>', 'Attachments')
-    .action((text, options) => {
-      post(hydrobond, text, options)
+    .action(async (text, options) => {
+      await post(hydrobond, text, options)
     })
 
   // Timeline
-  cli.command('timeline', 'Show timeline').action(() => {
-    timeline(hydrobond)
+  cli.command('timeline', 'Show timeline').action(async () => {
+    await timeline(hydrobond)
   })
 
   // Timeline steam
-  cli.command('stream', 'Show timeline with WebSocket').action(() => {
-    stream(hydrobond)
+  cli.command('stream', 'Show timeline with WebSocket').action(async () => {
+    await stream(hydrobond)
   })
 
   cli.parse()
